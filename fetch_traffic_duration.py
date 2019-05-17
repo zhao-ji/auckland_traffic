028d7b3c5859a6b9213489e8038c2eea91e0b77d
sydney <iamfrommop@gmail.com>
2019-05-17 14:28:37 +1200

    Issue #master - make celery schedule beats

--- fetch_traffic_duration.py
+++ fetch_traffic_duration.py
@@ -1,18 +1,19 @@
 from celery import Celery
+from celery.schedules import crontab
 from requests import get
 
 from local_settings import GOOGLE_URL, GOOGLE_API_KEY
 
 app = Celery("auckland_traffic")
 app.conf.update(
-    celery_timezone="Pacific/Auckland",
+    CELERY_TIMEZONE="Pacific/Auckland",
     BROKER_URL="redis://localhost",
-    celery_task_serializer="json",
-    celery_accept_content=["json"],
-    celerybeat_schedule={
+    CELERY_TASK_SERIALIZER="json",
+    CELERY_ACCEPT_CONTENT=["json"],
+    CELERYBEAT_SCHEDULE={
         "fetch_duration": {
             "task": "auckland_traffic.fetch_duration",
-            "schedule": 60.0,
+            "schedule": crontab(minute="*"),
             "options": {
                 "expires": 30,
                 "priority": 0,
