c9d41055de320797bb176d89b535e8bae8ea1ffd
Trevor.Max <Trevor.Max@btg.co.nz>
2019-05-20 16:02:23 +1200

    Issue #master - add more places into celery worker tasks

--- fetch_traffic_duration.py
+++ fetch_traffic_duration.py
@@ -4,6 +4,8 @@ from requests import get
 import sqlite3
 
 from local_settings import GOOGLE_URL, GOOGLE_API_KEY, REDIS_URL, DB_LOCATION
+from local_settings import HOME, OFFICE
+from local_settings import SCHOOL, CITY, DOMINION_ROAD, KOREAN_SHOP, BARBER_SHOP
 
 app = Celery("auckland_traffic")
 app.conf.update(
@@ -34,33 +36,48 @@ CREATE TABLE fetch_result (
 );
 """
 INSERT_SQL = """
-    insert into fetch_result(ID, Timestamp, Origin, Destination, Distance, Duration)
+    insert into
+    fetch_result(ID, Timestamp, Origin, Destination, Distance, Duration)
     values (NULL, DATETIME('now'),?,?,?,?);
 """
 
 
-@app.task(name="auckland_traffic.fetch_duration")
-def fetch_duration():
+def ping():
     params = {
+        "departure_time": "now",
+        "destinations": "|".join([SCHOOL, CITY, DOMINION_ROAD, KOREAN_SHOP, BARBER_SHOP]),
         "key": GOOGLE_API_KEY,
         "mode": "driving",
+        "origins": "|".join([HOME, OFFICE]),
         "region": "nz",
         "units": "metric",
-        "departure_time": "now",
         "traffic_model": "best_guess",
-        "origins": "60 stanhope road, auckland",
-        "destinations": "victoria street car park",
     }
     ret = get(GOOGLE_URL, params=params)
     if ret.ok:
-        status = ret.json()["rows"][0]["elements"][0]
-        distance = status["distance"]["value"]
-        duration = status["duration_in_traffic"]["value"]
-        with sqlite3.connect(DB_LOCATION, timeout=100) as sqlite_conn:
-            c = sqlite_conn.cursor()
-            c.execute(INSERT_SQL, ("home", "city", distance, duration))
-            print status["distance"]["text"], status["duration_in_traffic"]["text"]
-            sqlite_conn.commit()
+        data = ret.json()
+        for i in data["rows"]:
+            for j in i["elements"]:
+                yield j
+
+
+@app.task(name="auckland_traffic.fetch_duration")
+def fetch_duration():
+    result = ping()
+    insert_list = []
+
+    for origin in ["home", "office"]:
+        for destination in ["school", "city", "dominion_road", "korean_shop", "barber_shop"]:
+            data = result.next()
+            distance = data["distance"]["value"]
+            duration = data["duration_in_traffic"]["value"]
+            print origin, " => ", destination, data["distance"]["text"], data["duration_in_traffic"]["text"]
+            insert_list.append((origin, destination, distance, duration))
+
+    with sqlite3.connect(DB_LOCATION, timeout=100) as sqlite_conn:
+        c = sqlite_conn.cursor()
+        c.execute(INSERT_SQL, insert_list)
+        sqlite_conn.commit()
 
 
 if __name__ == "__main__":
