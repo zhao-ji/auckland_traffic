3b8bd3eedc9c87400e9c7574dc7da6cc45ba9a0f
Trevor.Max <Trevor.Max@btg.co.nz>
2019-05-20 14:58:08 +1200

    Issue #master - add three button into fronend chart, show the right date range

--- static/js/utils.js
+++ static/js/utils.js
@@ -132,3 +132,22 @@ window.chartColors = {
 
 	Samples.utils.srand(Date.now());
 }(this));
+
+
+function loadJSON(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
